dc4a0c9f08cdb4120d191457f01fe59b3af6974e
izzyisozaki <izzy_isozaki@hotmail.com>
2025-05-26 09:35:58 +0200

    Corrected duplicate case and queries

--- src/main/java/com/giuseppe/biblioteca/repository/BookRepository.java
+++ src/main/java/com/giuseppe/biblioteca/repository/BookRepository.java
@@ -2,6 +2,8 @@ package com.giuseppe.biblioteca.repository;
 
 import com.giuseppe.biblioteca.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -17,7 +19,13 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
     int countByAuthor(String author);
 
+    @Query("SELECT b FROM Book b ORDER BY b.publication_year DESC")
     List<Book> findAllByOrderByYearDesc();
 
-    List<Book> findByTitleOrAuthor(String keyword);
+    @Query("SELECT b FROM Book b WHERE LOWER(b.title) LIKE LOWER(CONCAT('%', :keyword, '%')) OR LOWER(b.author) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Book> findByTitleOrAuthor(@Param("keyword")String keyword);
+
+//    // Query personalizzate
+//    @Query("SELECT * FROM BOOK WHERE ID = :id")
+//    List<Book> getBooksById(Long id);
 }
